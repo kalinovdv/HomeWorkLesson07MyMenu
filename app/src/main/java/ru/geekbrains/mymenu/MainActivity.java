749b232ac637a76fc82f544a084b24a77b91f752
kalinovdv <kdv_ul@rambler.ru>
2021-07-14 11:56:37 +0400

    Изменил MainActivity.
    Добавил процедуры инициализации кнопок и процедуру вывода фрагмента.

--- app/src/main/java/ru/geekbrains/mymenu/MainActivity.java
+++ app/src/main/java/ru/geekbrains/mymenu/MainActivity.java
@@ -1,6 +1,10 @@
 package ru.geekbrains.mymenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +14,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        initButtonNotes();
+        initButtonAdd();
+        initButtonSettings();
+    }
+
+    private void initButtonSettings() {
+        AppCompatButton button = findViewById(R.id.buttonMainActivitySettings);
+        button.setOnClickListener(v -> {
+            showFragment(new AddFragment());
+        });
+    }
+
+    private void initButtonAdd() {
+        AppCompatButton button = findViewById(R.id.buttonMainActivityAddNote);
+        button.setOnClickListener(v -> {
+            showFragment(new AddFragment());
+        });
+    }
+
+    private void initButtonNotes() {
+        AppCompatButton button = findViewById(R.id.buttonMainActivityNotes);
+        button.setOnClickListener(v -> {
+            showFragment(new NotesFragment());
+        });
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
