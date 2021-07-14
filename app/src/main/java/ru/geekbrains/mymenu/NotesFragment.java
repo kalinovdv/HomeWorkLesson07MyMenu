36a0d382de32152fcbe2867200c2372d70d43cab
kalinovdv <kdv_ul@rambler.ru>
2021-07-14 23:33:23 +0400

    Добавил пункт меню "Добавить" в список заметок (NotesFragment).

--- app/src/main/java/ru/geekbrains/mymenu/NotesFragment.java
+++ app/src/main/java/ru/geekbrains/mymenu/NotesFragment.java
@@ -2,11 +2,16 @@ package ru.geekbrains.mymenu;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class NotesFragment extends Fragment {
 
@@ -15,7 +20,21 @@ public class NotesFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_notes, container, false);
-
+        setHasOptionsMenu(true);
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.notes_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.notesFragmentMenuAdd) {
+            Toast.makeText(getContext(), "Выбрано меню Добавить", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
