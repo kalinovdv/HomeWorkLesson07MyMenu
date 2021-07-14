9f45dd58e6a1e80aebc02429e987925f04914058
kalinovdv <kdv_ul@rambler.ru>
2021-07-14 14:01:28 +0400

    Добавил код для создания меню.

--- app/src/main/java/ru/geekbrains/mymenu/MainActivity.java
+++ app/src/main/java/ru/geekbrains/mymenu/MainActivity.java
@@ -2,11 +2,16 @@ package ru.geekbrains.mymenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +24,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        initToolbar();
         initButtonNotes();
         initButtonAdd();
         initButtonSettings();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.mainMenuSearch);
+        SearchView searchView = (SearchView) search.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+        return true;
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarMainActivity);
+        setSupportActionBar(toolbar);
     }
 
     private void initButtonSettings() {
