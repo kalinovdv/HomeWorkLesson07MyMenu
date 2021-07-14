56aef8e3ed75c4d7f82cbcad980273d696ee84fb
kalinovdv <kdv_ul@rambler.ru>
2021-07-15 00:01:09 +0400

    Добавил всплывающее меню.
    Добавил в список заметок (NotesFragment) всплывающее меню: "Добавить заметку", "Изменить заметку", "Удалить заметку", "Обновить список". Меню привязано к TextView.

--- app/src/main/java/ru/geekbrains/mymenu/NotesFragment.java
+++ app/src/main/java/ru/geekbrains/mymenu/NotesFragment.java
@@ -1,8 +1,8 @@
 package ru.geekbrains.mymenu;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +11,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class NotesFragment extends Fragment {
@@ -21,7 +23,39 @@ public class NotesFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_notes, container, false);
         setHasOptionsMenu(true);
+        initPopupMenu(view);
         return view;
+    }
+
+    private void initPopupMenu(View view) {
+        TextView textView = view.findViewById(R.id.textViewNotesFragment);
+        textView.setOnClickListener(v -> {
+            Activity activity = requireActivity();
+            PopupMenu popupMenu = new PopupMenu(activity, v);
+            activity.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    int id = item.getItemId();
+                    switch (id) {
+                        case R.id.popupMenuAdd:
+                            Toast.makeText(getContext(), "Добавить заметку", Toast.LENGTH_SHORT).show();
+                            return true;
+                        case R.id.popupMenuEdit:
+                            Toast.makeText(getContext(), "Изменить заметку", Toast.LENGTH_SHORT).show();
+                            return true;
+                        case R.id.popupMenuDelete:
+                            Toast.makeText(getContext(), "Удалить заметку", Toast.LENGTH_SHORT).show();
+                            return true;
+                        case R.id.popupMenuUpdate:
+                            Toast.makeText(getContext(), "Обновить список", Toast.LENGTH_SHORT).show();
+                            return true;
+                    }
+                    return true;
+                }
+            });
+            popupMenu.show();
+        });
     }
 
     @Override
