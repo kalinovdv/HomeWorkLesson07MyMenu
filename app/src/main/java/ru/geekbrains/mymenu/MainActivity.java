e62af40556de863ce77d5d1dc0cc5c831a54bc1d
kalinovdv <kdv_ul@rambler.ru>
2021-07-14 13:49:56 +0400

    Добавил новую activity_main.
    Добавил в новую activity_main контейнер CoordinatorLayout. Расположил в нем меню приложения (AppBarLayout), а ниже него подключил основное окно - content_main.

--- app/src/main/java/ru/geekbrains/mymenu/MainActivity.java
+++ app/src/main/java/ru/geekbrains/mymenu/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
 
         initView();
     }
