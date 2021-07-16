986e6525c43c88f0918c32fcfebcaa6406ed9b64
kalinovdv <kdv_ul@rambler.ru>
2021-07-16 14:47:41 +0400

    Добавил класс - содержимое карточки заметки.

---
+++ app/src/main/java/ru/geekbrains/mymenu/Note.java
@@ -0,0 +1,43 @@
+package ru.geekbrains.mymenu;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Date;
+
+public class Note {
+    private String titel;
+    private String text;
+    private Date date;
+
+    public Note(String titel, String text, Date date) {
+        this.titel = titel;
+        this.text = text;
+        this.date = date;
+    }
+
+    public String getTitel() {
+        return this.titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
