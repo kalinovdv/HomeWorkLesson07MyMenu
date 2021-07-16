0cb64c63a0bd66c4d06fa320e3a11d8266586cff
kalinovdv <kdv_ul@rambler.ru>
2021-07-16 16:37:06 +0400

    Добавил код в интерфейс для получения данных для карточек заметок.

---
+++ app/src/main/java/ru/geekbrains/mymenu/data/NotesSourceImpl.java
@@ -0,0 +1,41 @@
+package ru.geekbrains.mymenu.data;
+
+import android.content.res.Resources;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import ru.geekbrains.mymenu.NoteData;
+import ru.geekbrains.mymenu.R;
+
+public class NotesSourceImpl implements NotesSource{
+
+    private List<NoteData> dataSource;
+    private Resources resources;
+
+    public NotesSourceImpl(Resources resources) {
+        this.dataSource = new ArrayList<>(5);
+        this.resources = resources;
+    }
+
+    public NotesSourceImpl init() {
+        String[] titels = resources.getStringArray(R.array.titels);
+        String[] discription = resources.getStringArray(R.array.discription);
+
+        for (int i = 0; i < discription.length; i++) {
+            dataSource.add(new NoteData(titels[i], discription[i], new Date()));
+        }
+        return this;
+    }
+
+    @Override
+    public NoteData getNoteData(int position) {
+        return dataSource.get(position);
+    }
+
+    @Override
+    public int size() {
+        return dataSource.size();
+    }
+}
