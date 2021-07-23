099b4a7b2d7a24659fb52764f93962e7c32b7aa9
kalinovdv <kdv_ul@rambler.ru>
2021-07-23 12:09:17 +0400

    Изменил меню фрагмента notes_fragment.xml.
    Удалил пункты меню "Изменить" и "Удалить". Добавил обработку нажатий на пункты меню "Добавить" и "Очистить".

--- app/src/main/java/ru/geekbrains/mymenu/ui/NotesFragment.java
+++ app/src/main/java/ru/geekbrains/mymenu/ui/NotesFragment.java
@@ -1,32 +1,33 @@
 package ru.geekbrains.mymenu.ui;
 
-import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import ru.geekbrains.mymenu.AddFragment;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Date;
+
+import ru.geekbrains.mymenu.NoteData;
 import ru.geekbrains.mymenu.R;
 import ru.geekbrains.mymenu.data.NotesSource;
 import ru.geekbrains.mymenu.data.NotesSourceImpl;
 
 public class NotesFragment extends Fragment {
+
+    private NotesSource data;
+    private NotesAdapter adapter;
+    private RecyclerView recyclerView;
 
     public static NotesFragment newInstance() {
         return new NotesFragment();
@@ -37,21 +38,27 @@ public class NotesFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_notes, container, false);
-        RecyclerView recyclerView = view.findViewById(R.id.fragmentNotesRecyclerView);
-        NotesSource data = new NotesSourceImpl(getResources()).init();
-        initRecyclerView(recyclerView, data);
+        //RecyclerView recyclerView = view.findViewById(R.id.fragmentNotesRecyclerView);
+        data = new NotesSourceImpl(getResources()).init();
+        initView(view);
         setHasOptionsMenu(true);
-        initPopupMenu(view);
+        //initPopupMenu(view);
         return view;
     }
 
-    private void initRecyclerView(RecyclerView recyclerView, NotesSource data) {
+    private void initView(View view) {
+        recyclerView = view.findViewById(R.id.fragmentNotesRecyclerView);
+        data = new NotesSourceImpl(getResources()).init();
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
         recyclerView.setHasFixedSize(true);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
 
-        NotesAdapter adapter = new NotesAdapter(data);
+        adapter = new NotesAdapter(data);
         recyclerView.setAdapter(adapter);
 
         DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL);
@@ -103,13 +110,16 @@ public class NotesFragment extends Fragment {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.notesFragmentMenuAdd) {
-            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, new AddFragment());
-            fragmentTransaction.commit();
-            //Toast.makeText(getContext(), "Выбрано меню Добавить", Toast.LENGTH_SHORT).show();
-            return true;
+        switch (item.getItemId()) {
+            case R.id.notesFragmentMenuAdd:
+                data.addNoteData(new NoteData("Заметка " + (data.size() + 1), "Описание заметки " + (data.size() + 1), new Date()));
+                adapter.notifyItemInserted(data.size() - 1);
+                recyclerView.scrollToPosition(data.size() - 1);
+                return true;
+            case R.id.notesFragmentMenuClear:
+                data.clearNoteData();
+                adapter.notifyDataSetChanged();
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
