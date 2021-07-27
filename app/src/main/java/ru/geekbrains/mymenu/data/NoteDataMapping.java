2679f5aff8d31e0b235a7266c27c97db552d98a7
kalinovdv <kdv_ul@rambler.ru>
2021-07-27 15:10:16 +0400

    Добавил класс для работы с данными из FireBase.
    Добавил конверте данных из базы в карточку и обратно.

---
+++ app/src/main/java/ru/geekbrains/mymenu/data/NoteDataMapping.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.mymenu.data;
+
+import com.google.firebase.Timestamp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NoteDataMapping {
+    public static class Fields {
+        public static String DATE = "date";
+        public static String TITLE = "title";
+        public static String TEXT = "text";
+    }
+
+    public static NoteData toNoteData(String id, Map<String, Object> doc) {
+        Timestamp timestamp = (Timestamp) doc.get(Fields.DATE);
+        NoteData answer = new NoteData((String) doc.get(Fields.TITLE), (String) doc.get(Fields.TEXT), timestamp.toDate());
+        answer.setId(id);
+        return answer;
+    }
+
+    public static Map<String, Object> toDocument(NoteData noteData) {
+        Map<String, Object> answer = new HashMap<>();
+        answer.put(Fields.TITLE, noteData.getTitel());
+        answer.put(Fields.TEXT, noteData.getText());
+        answer.put(Fields.DATE, noteData.getDate());
+        return answer;
+    }
+}
---
+++ app/src/main/java/ru/geekbrains/mymenu/data/NotesSourceFirebaseImpl.java
@@ -0,0 +1,105 @@
+package ru.geekbrains.mymenu.data;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class NotesSourceFirebaseImpl implements NotesSource{
+
+    private static final String NOTES_COLLECTION = "notes";
+    private static final String TAG = "[NotesSourceFirebaseImpl]";
+
+    private FirebaseFirestore store = FirebaseFirestore.getInstance();
+
+    private CollectionReference collection = store.collection(NOTES_COLLECTION);
+
+    private List<NoteData> notesData = new ArrayList<NoteData>();
+
+    @Override
+    public NotesSource init(final NotesSourceResponse notesSourceResponse) {
+        collection.orderBy(NoteDataMapping.Fields.DATE, Query.Direction.DESCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @SuppressLint("LongLogTag")
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    notesData = new ArrayList<NoteData>();
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Map<String, Object> doc = document.getData();
+                        String id = document.getId();
+                        NoteData noteData = NoteDataMapping.toNoteData(id, doc);
+                        notesData.add(noteData);
+                    }
+                    Log.d(TAG, "данные получены: " + notesData.size());
+                } else {
+                    Log.d(TAG, "данные не получены: " + task.getException());
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @SuppressLint("LongLogTag")
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "ошибка: " + e);
+            }
+        });
+        return this;
+    }
+
+    @Override
+    public NoteData getNoteData(int position) {
+        return notesData.get(position);
+    }
+
+    @Override
+    public int size() {
+        if (notesData == null) {
+            return 0;
+        }
+        return notesData.size();
+    }
+
+    @Override
+    public void addNoteData(NoteData noteData) {
+        collection.add(NoteDataMapping.toDocument(noteData)).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                noteData.setId(documentReference.getId());
+            }
+        });
+    }
+
+    @Override
+    public void deleteNoteData(int position) {
+        collection.document(notesData.get(position).getId()).delete();
+        notesData.remove(position);
+    }
+
+    @Override
+    public void updateNoteData(int position, NoteData noteData) {
+        String id = noteData.getId();
+        collection.document(id).set(NoteDataMapping.toDocument(noteData));
+    }
+
+    @Override
+    public void clearNoteData() {
+        for (NoteData noteData : notesData) {
+            collection.document(noteData.getId()).delete();
+        }
+        notesData = new ArrayList<NoteData>();
+    }
+}
